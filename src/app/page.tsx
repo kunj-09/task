0228821b9ff447f1b2bbc4ff750d6c05b1982806
kunj-09 <kunj.mm@gmail.com>
2025-02-01 00:42:05 +0530

    error changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Image from "next/image";
+// import Image from "next/image";
 import { useState } from "react";
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
